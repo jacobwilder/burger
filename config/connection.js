fbc94f0d4d760302579c75aa4d55ddd641e2fc4a
Wilder8 <jacob.wilder826@gmail.com>
2019-11-05 18:23:28 -0500

    Implemented JAWSDBaddon/ deployed to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,22 @@
 var mysql = require("mysql");
 var dotenv = require("dotenv");
+var connection;
 
+// Configures dotenv package
 dotenv.config();
 
-var connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: '26Gamers8!',
-  database: 'burgers_db'
-});
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER, // FIXME: Establish a connection to your MySQL Database
+    password: process.env.DB_PW, // https://www.npmjs.com/package/mysql#establishing-connections
+    database: "burgers_db"
+  });
+};
 
 connection.connect(function(err) {
   if (err) {
